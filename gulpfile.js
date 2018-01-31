0288438dd69eef1610762430fb8f050a68160bf2
Rustem Ibragimov <garantkredo@gmail.com>
2018-01-31 13:24:48 +0500

    point1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'), //Подключаем Sass пакет
-    browserSync = require('browser-sync'); // Подключаем Browser Sync
+    browserSync = require('browser-sync'), // Подключаем Browser Sync
+    concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify      = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
 
 
 gulp.task('sass', function(){ // Создаем таск Sass
@@ -20,8 +22,21 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
     });
 });
 
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+        'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js', // Берем Magnific Popup
+        'app/libs/responsive-sticky-footer-plugin/dist/jquery.responsiveStickyFooter.js' // Берем Magnific Popup
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+
+    ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+});
+
+
+gulp.task('watch', ['browser-sync', 'scripts', 'sass'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass'], browserSync.reload); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
